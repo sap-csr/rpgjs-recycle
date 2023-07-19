f8b778e0afd0902967ac525bb6a6357af6707ab9
yspkm <yskim0830@outlook.com>
2023-07-19 12:00:38 +0000

    fix(build-and-deploy.sh): nginx 재실행 안하면 캐시된 html 그대로 들어가서 디자인 변경 딜레이 생김

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -8,12 +8,14 @@ echo "build end"
 echo ""
 
 echo "end-deploy begin"
+sudo systemctl stop nginx
 sudo docker container stop rpgjs-recycle 
 sudo docker container rm rpgjs-recycle 
+sudo systemctl start nginx
 echo "end-deploy done"
 
 echo ""
 
 echo "start-deploy begin"
-sudo docker container run --name rpgjs-recycle -p 3000:3000 -d rpg 
+sudo docker container run --name rpgjs-recycle -p 3000:3000 -d rpg:latest
 echo "start-deploy done"

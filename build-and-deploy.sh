89c26af2357ffbdd1cedc39ecf1723e05dfa665a
yspkm <yskim0830@outlook.com>
2023-07-19 14:59:13 +0900

    feat(build-and-deploy.sh): 기존 컨테이너 종료 시점을 빌드 이후로 변경해서 중단 없이 배포--> 가능하면 젠킨스 사용해서 쿠버에 올리면 좋은데 인스턴스 사용이 비쌈...

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+echo "build begin"
+git pull origin main
+sudo docker build -t rpg .
+echo "build end"
+
+echo ""
+
 echo "end-deploy begin"
 sudo docker container stop rpgjs-recycle 
 sudo docker container rm rpgjs-recycle 
 echo "end-deploy done"
 
-echo "build begin"
-git pull origin main
-sudo docker build -t rpg .
-echo "build end"
+echo ""
 
 echo "start-deploy begin"
 sudo docker container run --name rpgjs-recycle -p 80:3000 -p 443:3000 -d rpg 

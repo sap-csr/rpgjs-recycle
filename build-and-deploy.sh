5c0d74c5f9b97d4ada3fa3ff595c9c28388876af
yspkm <yskim0830@outlook.com>
2023-07-19 14:22:06 +0000

    chore():

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,20 +2,22 @@
 
 echo "build begin"
 git pull origin main
-sudo docker build -t rpg .
+#sudo docker image rm rpg
+sudo docker image build -t rpg .
 echo "build end"
 
 echo ""
 
 echo "end-deploy begin"
-sudo systemctl stop nginx
 sudo docker container stop rpgjs-recycle 
 sudo docker container rm rpgjs-recycle 
-sudo systemctl start nginx
 echo "end-deploy done"
 
 echo ""
 
 echo "start-deploy begin"
-sudo docker container run --name rpgjs-recycle -p 3000:3000 -d rpg:latest
+sudo systemctl stop nginx
+sudo docker container run --name rpgjs-recycle -p 3000:3000 -d rpg
+sudo systemctl start nginx
+sudo systemctl restart nginx
 echo "start-deploy done"
